8bb255bae11ce3097733101b50d87d0649d0f724
Jan-Willem Wisgerhof <jwwisgerhof@gmail.com>
2017-11-28 08:02:32 +1000

    Ease-of-use updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { HashRouter as Router, Route, Link } from "react-router-dom";
 
 import Users from "./components/Users";
 import Dashboard from "./components/Dashboard";
